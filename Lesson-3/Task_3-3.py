78c2a7c8d712c9affec2df500f36fc9ba4560552
Anton <Kaif@inbox.ru>
2021-08-29 22:44:40 +0500

    Task_3-3

--- Lesson-3/Task_3-3.py
+++ Lesson-3/Task_3-3.py
@@ -1,9 +1,15 @@
 # Реализовать функцию my_func(), которая принимает три позиционных аргумента,
 # и возвращает сумму наибольших двух аргументов.
-def summa(a, b, c):
-    n_list = {a, b, c}
+def summa(arg1, arg2, arg3):
+    """Возвращает сумму наибольших двух аргументов
+    Принимает три числа,
+    для некорректных аргументов возвращает None"""
+    try:
+        n_list = [int(arg1), int(arg2), int(arg3)]
+    except ValueError:
+        return None
     n_list.remove(min(n_list))
     return sum(n_list)
 
 
-print(f'Result: {summa(-45, 3, 2)}')
+print(summa(-1, '2', 3))
